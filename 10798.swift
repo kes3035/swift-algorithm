7c992da563b98b736a14b176be6be30fdbb73bd8
kes3035 <kes3035@gmail.com>
2022-11-14 01:52:21 +0900

    11/14/10798/fin/runtimeError

--- 10798.swift
+++ 10798.swift
@@ -1,24 +1,38 @@
-//다섯줄의 입력을 받아 각 배열의 0번째 인덱스들,1번째 인덱스들을 모아 새로운 배열 완성 후 출력하기
-
-var resultArr: [[String]] = []
-var newArr: [String] = []
-for _ in 0...4 {
-    let arr: String = readLine()!
-    var charArr: [String] = []
-    for char in arr {
-        charArr.append(String(char))
+var resultArr: [String] = []
+var maxArr: [Int] = []
+var arr: [[String]] = []
+var stringArr: [String] = []
+for _ in 1...5 {
+    let input = readLine()!
+    for char in input {
+        stringArr.append(String(char))
     }
-    resultArr.append(charArr)
+    arr.append(stringArr)
+    stringArr = []
 }
-// [ [A,A,B,C,D,D], [a,f,z,z], [0,9,1,2,1], [a,8,E,W,g,6], [P,5,h,3,k,x] ]
-repeat {
-    for i in 0...4 {
-        if resultArr[i].count != 0 {
-            var firstString = resultArr[i].first!
-            newArr.append(firstString)
-            resultArr[i].remove(at: 0)
+
+
+for i in 0...4 {
+    maxArr.append(arr[i].count)
+}
+let maxNum = maxArr.sorted(by: >)[0]
+for i in 0...4 {
+    if arr[i].count != maxNum {
+        for _ in 1...(maxNum - arr[i].count) {
+            arr[i].append("@")
         }
     }
-} while ( resultArr[0].isEmpty && resultArr[1].isEmpty && resultArr[2].isEmpty && resultArr[3].isEmpty && resultArr[4].isEmpty )
-print(resultArr)
-print(newArr)
+}
+for i in 0..<maxNum {
+    for j in 0..<maxNum {
+        if arr[j][i] == "@" {
+            continue
+        }
+        else {
+            resultArr.append(arr[j][i])
+        }
+    }
+}
+for i in 0..<resultArr.count {
+    print(resultArr[i], terminator: "")
+}
