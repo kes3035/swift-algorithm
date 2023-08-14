916b1a221a1743be05e396a12264f48527d3ee91
kes3035 <116807969+kes3035@users.noreply.github.com>
2023-08-14 18:58:11 +0900

    [unrated] Title: 대소문자 바꿔서 출력하기, Time: 5.53 ms, Memory: 12.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let s1 = readLine()!
+
+for character in s1 {
+    if character.isUppercase {
+        print(String(character).lowercased(), terminator: "")
+    } else {
+        print(String(character).uppercased(), terminator: "")
+
+    }
+}
