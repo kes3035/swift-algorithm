c508441518f198965d894d5ed23f9557ffec3108
kes3035 <116807969+kes3035@users.noreply.github.com>
2024-06-27 16:20:14 +0900

    [level 0] Title: 0 떼기, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ n_str:String) -> String {
+    var result = n_str
+    for char in n_str {
+        if char == "0" {
+            result.removeFirst()
+        } else {
+            break
+        }
+    }
+    return result
+}
